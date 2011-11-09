5cdf0561a310781f409b93c9a7938f6fb5a67fde
PhillipKregg <phillipkregg@gmail.com>
2011-11-09 16:20:18 -0600

    Added Modal popup for 'new' and 'edit' methods on Contact model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  # in your app controller, you'll want to set the layout to nil for XHR (ajax) requests
+  layout Proc.new { |controller| controller.request.xhr? ? nil : 'application' }
+  
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,12 +4,64 @@
 
  $(document).ready(function(){
    
+   
+// Top Navigation - finds which tab you are on   
     $(function(){
 	   var path = location.pathname.substring(1);
 	   if ( path )
 	     $('#nav_list li a[href$="' + path + '"]').attr('class', 'nav_select');
  	});
    
+ 
+//  Modal Window 
+	$(function(){
+	  var $modal = $('#modal'),
+	      $modal_close = $modal.find('.close'),
+	      $modal_container = $('#modal-container');
+	
+	  // This bit can be confusing. Since Rails.js sends an accept header asking for
+	  // javascript, but we want it to return HTML, we need to override this instead.
+	  $('a[data-remote]').live('ajax:beforeSend', function(e, xhr, settings){
+	    xhr.setRequestHeader('accept', '*/*;q=0.5, text/html, ' + settings.accepts.html);
+	  });
+	
+	  // Handle modal links with the data-remote attribute
+	  $('a[data-remote]').live('ajax:success', function(xhr, data, status){
+	    $modal
+	      .html(data)
+	      .prepend($modal_close)
+	      .css('top', $(window).scrollTop() + 40)
+	      .show();
+	    $modal.fadeIn("fast");
+	    
+	    $modal_container.show();
+	   
+	  });
+	
+	  // Hide close button click
+	  $('.close', '#modal').live('click', function(){
+	    $modal_container.hide();
+	    $modal.hide();
+	    return false;
+	  });	 
+	  
+	  	  
+	});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  });
 
 
